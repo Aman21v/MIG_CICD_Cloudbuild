1292f4bdb6ba01bb6c111a329a1f635d64225215
Aman21v <90607017+Aman21v@users.noreply.github.com>
2022-08-29 18:29:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
    name = os.environ.get("NAME", "World")
-   return "Hello from A to {}!, Welcome to Test part 3333".format(name)
+   return "Hello from A to ee{}!, Welcome to Test part 3333".format(name)
  
 if __name__ == "__main__":
    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
